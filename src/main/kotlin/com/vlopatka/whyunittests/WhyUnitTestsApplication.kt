32b8b106534666e295a58d7ed349ce02563ae961
vlopatka <vlopatka@overonix.com>
2020-09-28 18:34:08 +0300

    init

---
+++ src/main/kotlin/com/vlopatka/whyunittests/WhyUnitTestsApplication.kt
@@ -0,0 +1,11 @@
+package com.vlopatka.whyunittests
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WhyUnitTestsApplication
+
+fun main(args: Array<String>) {
+	runApplication<WhyUnitTestsApplication>(*args)
+}
---
+++ src/test/kotlin/com/vlopatka/whyunittests/WhyUnitTestsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vlopatka.whyunittests
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WhyUnitTestsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
