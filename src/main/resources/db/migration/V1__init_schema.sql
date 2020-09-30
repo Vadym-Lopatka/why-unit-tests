a1c1c8b7d7c95de6bd856ff0db717a0b331abfa5
vlopatka <vlopatka@overonix.com>
2020-09-30 12:43:41 +0300

    [01] - fixed flyway

--- src/main/resources/db/migration/V1__init_schema.sql
+++ src/main/resources/db/migration/V1__init_schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE traffic_light (
     uuid character varying(39) NOT NULL,
     address  character varying(255) NOT NULL,
     state  character varying(255) NOT NULL,
-    indicator  character varying(255) NOT NULL
+    indicator  character varying(255) NOT NULL,
 
     PRIMARY KEY (uuid)
 );
