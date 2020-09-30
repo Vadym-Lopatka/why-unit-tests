af1aca1102f154d8de5b6e061c3d6964fde5855e
vlopatka <vlopatka@overonix.com>
2020-09-30 15:23:22 +0300

    [02] - renamed app

--- src/main/kotlin/com/vlopatka/App.kt
+++ src/main/kotlin/com/vlopatka/App.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class WhyUnitTestsApplication
+class App
 
 fun main(args: Array<String>) {
-    runApplication<WhyUnitTestsApplication>(*args)
+    runApplication<App>(*args)
 }
